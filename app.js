ecb06f9127ec451de3628be43575822dbbf1f2c7
Gyeongjae KIM <kimg009263@gmail.com>
2022-08-16 19:03:29 +1200

    last deployment

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ const books = require('./routes/api/books');
 
 const app = express();
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, './cise_ass1a_worksheet3/build')))
-
-    app.get("*", (req, res)=> {
-        res.sendFile(path.join(__dirname,'cise_ass1a_worksheet3/build' ,'index.html'))
-    })
-    
-}
-else {
-    app.get('/',(req,res)=>{
-        res.send("Api running!!");
-    });
-}
-
 // Connect Database
 connectDB();
 
@@ -33,10 +19,24 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, './cise_ass1a_worksheet3/build')))
+
+    app.get('*', (req, res)=> {
+        res.sendFile(path.join(__dirname,'./cise_ass1a_worksheet3/build' ,'index.html'))
+    })
+    
+}
+else {
+    app.get('/',(req,res)=>{
+        res.send("Api running!!");
+    });
+}
 
 const port = process.env.PORT || 8082;
 
--- config/db.js
+++ config/db.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+require("dotenv").config()
+
 const connectDB = async () => {
   try {
     await mongoose.connect(
       db,
       {
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
       }
     );
 
-    console.log('MongoDB is Connected...');
+    console.log('MongoDB is Connected…');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
